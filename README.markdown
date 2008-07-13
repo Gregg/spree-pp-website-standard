42f2cffd0c24db979f4c1646a6553056cde3ff87
Sean Schofield <sean@endpoint.com>
2008-07-13 19:39:10 -0400

    Updated pseudo code.

--- README.markdown
+++ README.markdown
@@ -1,4 +1,10 @@
-= Spree Pp Website Standard
+# Spree Pp Website Standard
 
 TODO: CheckoutController.rb rewrite
 TODO: Show a user's purchases (screencasts) on their My Account page.
+
+# Installation 
+
+<pre>
+script/extension install git://github.com/Gregg/spree-pp-website-standard.git  
+</pre>
--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -3,54 +3,64 @@ class CheckoutController < Spree::BaseController
   # You can send in test notifications on the developer page here:
   # https://developer.paypal.com/us/cgi-bin/devscr?cmd=_ipn-link-session
   def notify
-    # make sure payment status is successful
+    # notify = Paypal::Notification.new(request.raw_post)
+
+    # if there is a cart record with a reference_hash that matches "invoice"=>"fe130c554f6497a65de238b483c3a3754676a43d"
+      # begin transaction
+        # Create an order from the cart (include the user if cart has one)
+        # Create a payment for the order
+        # Remove the cart from the session
+        # Destroy the cart (optimistic locking for the cart in case notify is racing us)
+      # end transaction
+    # else
+      # return must have come in first
+      # find the order
+    # end
     
-    # if "payment_status"=>"Completed"
+    # if notify.acknowledge
+      #begin
+      #  case notify.status
+      #  when "Completed" 
+      #    
+      #    
+      #  when "Pending" 
+      #    
+      #    
+      #  else
+      #    logger.error("Failed to verify Paypal's notification, please investigate")
+      #  end
+      #ensure
+      #  save order
+    # else
+      # log error
+      # create an error transaction
+      # change order status
+      # save order and transaction
+    # end
     
-          # Look up the cart based on the reference_hash hidden in invoice
-          # "invoice"=>"fe130c554f6497a65de238b483c3a3754676a43d"
-          # If cart exists, and the "payment_gross"=>"16.99" is the same what the cart says 
-          
-            # If user is set on cart, 
-              # set this on the order
-              # send them an email to let them know their order is ready, and they can go to my_account page to get it
-            # else
-              # grab their paypal email address...   "payer_email"=>"buyer@paypalsandbox.com"
-              # Send them an email with a link to the return page below, with the invoice in the url
-            # end
-          # end
-          
-     # end
+    # call notify hook (which will email users, etc.)
   end
   
   # When they've returned from paypal
   def return
-    # Should be sent the invoice information.  "invoice"=>"fe130c554f6497a65de238b483c3a3754676a43d"
     
-    # Remove the session cart_id when a user hits this page, but don't try to delete the cart, incase it's waiting to be notified (above)
+    # if there is a cart record with a reference_hash that matches "invoice"=>"fe130c554f6497a65de238b483c3a3754676a43d"
+      # begin transaction
+        # Create an order from the cart
+        # Create a payment for the order
+        # Remove the cart from the session
+        # Destroy the cart (optimistic locking for the cart in case notify is racing us)
+      # end transaction
+    # else (notify must have come in first)
+      # Find the order
+    # end
+
+    # Add a transaction to the payment to record the information posted in the return 
+    # save order and transaction 
     
-    # If this invoice has already been completed
-          # if user is logged in
-              # set a flash "Thanks for your order, you can download it below", and send them to their "my_account" page
-          # else
-              # is the order is associated with a user?
-                  # set flash, looks like you just need to login to access your videos
-                  # redirect to my accounts page (which will then load login page)
-              # else
-                  # set flash, "Please just create an account and you can download your heart away"
-                  # set session[:return_to] to thir my_account page
-                  # redirect to user create form.
-              # end
-          # end
-     # else
-        # if user is logged in OR the cart already has a user_id
-          # Doesn't look like your payment has been processed quite yet, you should receive an email when it does, or just refresh this page
-          # show return.html.erb.. default
-        # else 
-            # Thanks for your order, it doesn't look like your payment has processed quite yet, please take a moment to create an account
-            # so we can let you know when it's done, and so you can have permanent download access to your screencasts
-        # end
-     # end
+    # Call the return hook (Application specific email, flash messages and redirects.)    
+
+    # Render thank you (unless redirected by hook of course)
   end
   
 end
