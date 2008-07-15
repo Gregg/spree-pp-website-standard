0394e9cd0898489bf85470e273714a168f8f5a87
Sean Schofield <schof@apache.org>
2008-07-14 22:07:48 -0400

    Flushed out notify tests a little more.  Started implementing the order lookup/creation code.

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -7,17 +7,19 @@ class CheckoutController < Spree::BaseController
   def notify
     notify = Paypal::Notification.new(request.raw_post)
 
-    # if there is a cart record with a reference_hash that matches "invoice"=>"fe130c554f6497a65de238b483c3a3754676a43d"
-      # begin transaction
+    # Check to see if there is a cart record matching the invoice hash
+    if cart = Cart.find_by_reference_hash(notify.invoice)      
+      Order.transaction do
+        @order = Order.new_from_cart(cart)
         # Create an order from the cart (include the user if cart has one)
         # Create a payment for the order
         # Remove the cart from the session
         # Destroy the cart (optimistic locking for the cart in case notify is racing us)
-      # end transaction
-    # else
+      end
+    else
       # return must have come in first
       # find the order
-    # end
+    end
     
     if notify.acknowledge
       #begin
@@ -33,6 +35,7 @@ class CheckoutController < Spree::BaseController
       #  end
       #ensure
       #  save order
+      #end
     else
       # log error
       # create an error transaction
--- spec/controllers/checkout_controller_spec.rb
+++ spec/controllers/checkout_controller_spec.rb
@@ -3,11 +3,15 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe CheckoutController do
   describe "/notify" do
 
+    before(:each) do
+      @mock_hash = "M0CKH4SH"
+      @notification = mock("IPN Notification", :invoice => @mock_hash, :acknowledge => true)
+      ActiveMerchant::Billing::Integrations::Paypal::Notification.stub!(:new).with(any_args).and_return @notification
+    end
+
     describe "notifications in general", :shared => true do
       
       before(:each) do
-        @notification = mock("IPN Notification")
-        ActiveMerchant::Billing::Integrations::Paypal::Notification.stub!(:new).with(any_args).and_return @notification
       end
       
       it "should acknowledge the notification" do
@@ -15,17 +19,23 @@ describe CheckoutController do
         post :notify
       end
       
-      describe "when acknowledgement succedes" do
+      describe "when acknowledgement succeeds" do
         
         before(:each) do
           @notification.should_receive(:acknowledge).and_return(true)
         end
 
         describe "when status is completed" do
-          # TODO - add more specs here
+          it "should verify the order total"
+          it "should change the order status to paid"
+          it "should create a transaction to reflect the successful verification"
+          it "should save the order"
         end
         describe "when status is pending" do
-          # TODO - add more specs here
+          it "should verify the order total"
+          it "should change the order status to pending payment"
+          it "should create a transaction to reflect the pending notification"
+          it "should save the order"
         end
       end
 
@@ -36,21 +46,42 @@ describe CheckoutController do
         end
         
         it "should create an error transaction"
-        it "should change the order status to ________"
+        it "should change the order status to incomplete"
         it "should save the order"
       end
     end
     
     describe "before return" do
-      it "should locate the cart using the reference hash"
-      it "should create an order from the cart"
+      
+      before :each do
+        @mock_cart = mock_model(Cart)
+        Cart.stub!(:find_by_reference_hash).with(any_args).and_return(@mock_cart)
+        @mock_order = mock_model(Order)
+        Order.stub!(:new_from_cart).with(any_args).and_return(@mock_order)  
+      end
+      
+      it "should locate the cart using the reference hash" do
+        Cart.should_receive(:find_by_reference_hash).with(@mock_hash).and_return(@mock_cart)
+        post :notify
+      end
+      
+      it "should create an order from the cart" do
+        Order.should_receive(:new_from_cart).with(@mock_cart).and_return(@mock_order)
+        post :notify
+      end
+      
       it "should create a payment for the order"
       it "should remove the cart from the session"
       it "should destroy the cart"
-      it_should_behave_like "notifications in general"
+      #it_should_behave_like "notifications in general"
     end
     
     describe "after return" do
+      
+      before :each do
+        Cart.should_receive(:find_by_reference_hash).with(@mock_hash).and_return(nil)
+      end
+      
       it "should find the order using the reference hash"
       it_should_behave_like "notifications in general"
     end
